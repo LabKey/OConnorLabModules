528dd28cf0b29990923f991d7b6d30a6337a5da3
Ryan Standley <rstandley@labkey.com>
2014-07-22 22:51:47 +0000

    now with web directory
    SVN r33523 |2014-07-22 22:51:47 +0000

---
+++ OConnor/resources/web/oconnor/oconnorAlabrityConfig.js
@@ -0,0 +1,4 @@
+//this file contains site-specific settings to make alabrity modules portable to other LabKey systems, like the WNPRC EHR
+
+var dbSchemaName = 'oconnor';
+var moduleName = 'oconnor';
