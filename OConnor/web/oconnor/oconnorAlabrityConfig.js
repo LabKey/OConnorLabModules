67c5d2260fd37f30bdbad0125131f09ba55d824f
Ryan Standley <rstandley@labkey.com>
2014-07-22 22:40:15 +0000

    fix initial checkin of oconnor module
    SVN r33522 |2014-07-22 22:40:15 +0000

--- OConnor/web/oconnor/oconnorAlabrityConfig.js
+++
@@ -1,4 +0,0 @@
-//this file contains site-specific settings to make alabrity modules portable to other LabKey systems, like the WNPRC EHR
-
-var dbSchemaName = 'oconnor';
-var moduleName = 'oconnor';
