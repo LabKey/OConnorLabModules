e18b5e53a189d8faa60dc9f05fe89ff60e920a50
Adam Rauch <adam@labkey.com>
2014-08-21 01:09:54 +0000

    Update licenses
    SVN r33989 |2014-08-21 01:09:54 +0000

--- OConnor/resources/schemas/dbscripts/postgresql/oconnor-0.00-1.07.sql
+++ OConnor/resources/schemas/dbscripts/postgresql/oconnor-0.00-1.07.sql
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 David O'Connor
+ * Copyright (c) 2014 LabKey Corporation
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
