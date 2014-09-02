6022408e1228a363a2ee4bc310dedc5285980549
Ryan Standley <rstandley@labkey.com>
2014-09-02 17:31:23 +0000

    fix for test/data mismatch
    SVN r34167 |2014-09-02 17:31:23 +0000

--- OConnorExperiments/test/src/org/labkey/test/tests/OConnorListTest.java
+++ OConnorExperiments/test/src/org/labkey/test/tests/OConnorListTest.java
@@ -10,6 +10,7 @@ import org.labkey.test.TestFileUtils;
 import org.labkey.test.categories.CustomModules;
 import org.labkey.test.categories.OConnor;
 import org.labkey.test.util.LogMethod;
+import org.labkey.test.util.PortalHelper;
 import org.labkey.test.util.PostgresOnlyTest;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
@@ -30,9 +31,9 @@ public class OConnorListTest extends BaseWebDriverTest implements PostgresOnlyTe
     private static String PROJECT_NAME = "OConnorTestProject";
     private static String FOLDER_ZIP_FILE = "OConnor_Test.folder.zip";
     private static String[] MODULES = {"OConnor", "OConnorExperiments"};
-    private static String[] SAMPLE_TYPES = {"type1", "type2", "type3", "type4"};
+    private static String[] SPECIMEN_TYPES = {"type1", "type2", "type3", "type4"};
     private static String[] EXPERIMENT_TYPES = {"name1", "name2", "name3", "name4"};
-    private static String[] DISABLED_SAMPLE_TYPES = {"disabledtype1", "disabledtype2"};
+    private static String[] DISABLED_SPECIMEN_TYPES = {"disabledtype1", "disabledtype2"};
     private static String[] DISABLED_EXPERIMENT_TYPES = {"disabledname1", "disabledname2"};
 
     @Nullable
@@ -64,17 +65,50 @@ public class OConnorListTest extends BaseWebDriverTest implements PostgresOnlyTe
 
     private void setupOConnorProject()
     {
+        PortalHelper portalHelper = new PortalHelper(this);
         _containerHelper.createProject(getProjectName(), "OConnor Purchasing System");
         _containerHelper.enableModules(Arrays.asList(MODULES));
         //TODO: turn query validation back on once query validation bugs are fixed
         importFolderFromZip(TestFileUtils.getSampleData(FOLDER_ZIP_FILE), false, 1);
+        goToProjectHome();
+//        portalHelper.addQueryWebPart("experimentType", "oconnor", "experiment_types", null);
+//        portalHelper.addQueryWebPart("specimenType", "oconnor", "specimen_type", null);
+//        for(String enabledSpecimenType : SPECIMEN_TYPES)
+//        {insertSpecimenType(enabledSpecimenType, true);}
+//        for(String disabledSpecimenType : DISABLED_SPECIMEN_TYPES)
+//        {insertSpecimenType(disabledSpecimenType, false);}
+//        for(String enabledExperimentType : EXPERIMENT_TYPES)
+//        {insertExperimentType(enabledExperimentType, true);}
+//        for(String disabledExperimentType : DISABLED_EXPERIMENT_TYPES)
+//        {insertExperimentType(disabledExperimentType, false);}
+        goToSchemaBrowser();
+        shortWait().until(ExpectedConditions.elementToBeClickable(Locator.xpath("//span[.='OConnorExperiments']").toBy()));
+        click(Locator.xpath("//span[.='OConnorExperiments']"));
+        shortWait().until(ExpectedConditions.elementToBeClickable(Locator.linkWithSpan("ExperimentType").toBy()));
+        click(Locator.linkWithSpan("ExperimentType"));
+        click(Locator.linkWithText("view data"));
+        for(String type : EXPERIMENT_TYPES)
+        {
+            waitForElement(Locator.linkWithSpan("Insert New"));
+            click(Locator.linkWithSpan("Insert New"));
+            setFormElement(Locator.name("quf_Name"), type);
+            click(Locator.linkWithSpan("Submit"));
+        }
+        for(String type : DISABLED_EXPERIMENT_TYPES)
+        {
+            waitForElement(Locator.linkWithSpan("Insert New"));
+            click(Locator.linkWithSpan("Insert New"));
+            setFormElement(Locator.name("quf_Name"), type);
+            uncheckCheckbox(Locator.checkboxByName("quf_Enabled"));
+            click(Locator.linkWithSpan("Submit"));
+        }
     }
 
     @Test
     public void testImportedListValues()
     {
         goToProjectHome();
-        for(String type : SAMPLE_TYPES)
+        for(String type : SPECIMEN_TYPES)
         {
             assertElementPresent(Locator.linkWithText(type));
         }
@@ -88,16 +122,19 @@ public class OConnorListTest extends BaseWebDriverTest implements PostgresOnlyTe
         //check available experiment types
         click(Locator.linkWithText("Insert New"));
         waitForElement(Locator.linkWithText("history"));
-        List<String> options = _ext4Helper.getComboBoxOptions("Experiment Type:");
+        click(Locator.xpath("//div[contains(@class, 'x4-trigger-index')]"));
+//        List<String> options = new ArrayList<>();
+//        List<WebElement> optionsEls = this.getDriver().findElements(By.className("list-item"));
+//        for(WebElement optionEl : optionsEls){options.add(optionEl.getText());}
         //enabled types are present
         for(String option : EXPERIMENT_TYPES)
         {
-            assert(options.contains(option));
+            assertTextPresent(option);
         }
         //disabled types are not shown
         for(String d_option : DISABLED_EXPERIMENT_TYPES)
         {
-            assert(!options.contains(d_option));
+            assertTextNotPresent(d_option);
         }
         //check available specimen types
         beginAt("/oconnor/OConnorTestProject/inventory_specimen_available.view?");
@@ -107,17 +144,17 @@ public class OConnorListTest extends BaseWebDriverTest implements PostgresOnlyTe
         waitForText("Specimen Details");
         click(Locator.id("specimen_type"));
         waitForText("type1");
-        for(String option : SAMPLE_TYPES)
+        for(String option : SPECIMEN_TYPES)
         {
             //assert(options.contains(option));
             assertTextPresent(option);
         }
         //disabled types are not shown
-        for(String d_option : DISABLED_SAMPLE_TYPES)
+        for(String d_option : DISABLED_SPECIMEN_TYPES)
         {
             //assert(!options.contains(d_option));
             //TODO: re-enable this test once bug is fixed
-            //assertTextNotPresent(d_option);
+            assertTextNotPresent(d_option);
             //assertElementNotPresent(Locator.xpath("//div[@class='x-combo-list-item'][.='" + d_option + "']"));
         }
     }
@@ -127,5 +164,29 @@ public class OConnorListTest extends BaseWebDriverTest implements PostgresOnlyTe
     {
         //skip query validation, queries depend on user defined lists and modules to be available in specific containers
         //TODO: update archived project in sampledata to set up project correctly
+    }
+
+    //assumes query webpart named "experimentType" is visible
+    private void insertExperimentType(String type, Boolean enabled )
+    {
+        PortalHelper portalHelper = new PortalHelper(this);
+        shortWait().until(ExpectedConditions.elementToBeClickable(Locator.linkWithSpan("Insert New").toBy()));
+        portalHelper.clickWebpartMenuItem("experimentType", "Insert New");
+        setFormElement(Locator.input("quf_Name"), type);
+        if(!enabled){uncheckCheckbox(Locator.checkboxByName("quf_enabled"));}
+        if(enabled){checkCheckbox(Locator.checkboxByName("quf_enabled"));}
+        click(Locator.linkWithSpan("Submit"));
+    }
+
+    //assumes query webpart named "specimenType" is visible
+    private void insertSpecimenType(String type, Boolean enabled)
+    {
+        PortalHelper portalHelper = new PortalHelper(this);
+        shortWait().until(ExpectedConditions.elementToBeClickable(Locator.linkWithSpan("Insert New").toBy()));
+        portalHelper.clickWebpartMenuItem("specimenType", "Insert New");
+        setFormElement(Locator.input("quf_specimen_type"), type);
+        if(!enabled){uncheckCheckbox(Locator.checkboxByName("quf_enabled"));}
+        if(enabled){checkCheckbox(Locator.checkboxByName("quf_enabled"));}
+        click(Locator.linkWithSpan("Submit"));
     }
 }
