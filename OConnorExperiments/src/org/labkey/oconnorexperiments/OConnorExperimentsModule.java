eb95f8756c4864ec6c5233c270db1059502b3d17
Adam Rauch <adam@labkey.com>
2024-12-31 04:32:07 -0800

    Bump schema versions to 25.000 (#154)

--- OConnorExperiments/src/org/labkey/oconnorexperiments/OConnorExperimentsModule.java
+++ OConnorExperiments/src/org/labkey/oconnorexperiments/OConnorExperimentsModule.java
@@ -62,7 +62,7 @@ public class OConnorExperimentsModule extends DefaultModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 24.000;
+        return 25.000;
     }
 
     @Override
