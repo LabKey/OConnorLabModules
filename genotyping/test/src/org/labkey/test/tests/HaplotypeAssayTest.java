3e5a31fcaba48c144baac7c85d825f3166ab64a9
labkey-jeckels <jeckels@labkey.com>
2025-06-28 09:18:40 -0700

    Fix build

--- genotyping/test/src/org/labkey/test/tests/HaplotypeAssayTest.java
+++ genotyping/test/src/org/labkey/test/tests/HaplotypeAssayTest.java
@@ -25,10 +25,10 @@ import org.labkey.test.TestTimeoutException;
 import org.labkey.test.categories.CustomModules;
 import org.labkey.test.categories.OConnor;
 import org.labkey.test.components.DomainDesignerPage;
+import org.labkey.test.components.assay.AssayConstants;
 import org.labkey.test.components.domain.DomainFormPanel;
 import org.labkey.test.pages.ReactAssayDesignerPage;
 import org.labkey.test.params.FieldDefinition;
-import org.labkey.test.tests.study.AssayTest;
 import org.labkey.test.util.DataRegionTable;
 import org.labkey.test.util.Ext4Helper;
 import org.labkey.test.util.ListHelper;
@@ -588,7 +588,7 @@ public class HaplotypeAssayTest extends GenotypingBaseTest
     {
         log("Importing Haplotype Run: " + assayId);
         goToAssayImport(assayName);
-        setFormElement(AssayTest.ASSAY_NAME_FIELD_LOCATOR, assayId);
+        setFormElement(AssayConstants.ASSAY_NAME_FIELD_LOCATOR, assayId);
         checkCheckbox(Locator.name("enabled"));
         selectOptionByText(Locator.name("speciesId"), "mamu");
         // NOTE: consider breaking these into seperate tests...
