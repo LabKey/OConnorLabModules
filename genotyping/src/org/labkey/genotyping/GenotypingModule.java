12f31cf2887f6087e456353fd266c68ef4f77d22
Adam Rauch <adam@labkey.com>
2026-01-01 09:00:26 -0800

    Bump schema versions to 26.000 (#189)

--- genotyping/src/org/labkey/genotyping/GenotypingModule.java
+++ genotyping/src/org/labkey/genotyping/GenotypingModule.java
@@ -51,7 +51,7 @@ public class GenotypingModule extends DefaultModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 25.000;
+        return 26.000;
     }
 
     @Override
