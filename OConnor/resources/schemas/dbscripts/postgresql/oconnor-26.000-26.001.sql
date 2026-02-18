dcadb15ca80392155393af6d76305d1a9260ead6
Binal Patel <binalp@labkey.com>
2026-02-18 10:34:22 -0800

    Move the CREATE VIEW statements from oconnor-0.000-23.000.sql into oconnor-create.sql. Add a drop script.

--- OConnor/resources/schemas/dbscripts/postgresql/oconnor-26.000-26.001.sql
+++ OConnor/resources/schemas/dbscripts/postgresql/oconnor-26.000-26.001.sql
@@ -5,7 +5,7 @@ BEGIN
 FOR rec IN
 SELECT c.table_name, c.column_name
 FROM information_schema.columns c
-         JOIN information_schema.tables t ON t.table_schema = c.table_schema AND t.table_name = c.table_name
+JOIN information_schema.tables t ON t.table_schema = c.table_schema AND t.table_name = c.table_name
 WHERE c.table_schema = 'oconnor'
   AND t.table_type = 'BASE TABLE'
   AND lower(c.column_name) IN ('createdby',
@@ -23,43 +23,20 @@ END $$;
 DO $$
   DECLARE
 rec RECORD;
-    view_rec RECORD;
 BEGIN
-    -- Save view definitions and drop all views in oconnor schema to clear dependencies
-    CREATE TEMP TABLE _oconnor_view_defs (viewname text, definition text);
-
-FOR view_rec IN
-SELECT viewname, definition
-FROM pg_views
-WHERE schemaname = 'oconnor'
-    LOOP
-INSERT INTO _oconnor_view_defs VALUES (view_rec.viewname, view_rec.definition);
-EXECUTE format('DROP VIEW IF EXISTS oconnor.%I CASCADE', view_rec.viewname);
-END LOOP;
-
-    -- Alter container columns on base tables
 FOR rec IN
 SELECT c.table_name, c.column_name
 FROM information_schema.columns c
-         JOIN information_schema.tables t ON t.table_schema = c.table_schema AND t.table_name = c.table_name
+JOIN information_schema.tables t ON t.table_schema = c.table_schema AND t.table_name = c.table_name
 WHERE c.table_schema = 'oconnor'
   AND t.table_type = 'BASE TABLE'
   AND lower(c.column_name) = 'container'
   AND c.data_type = 'character varying'
     LOOP
-        EXECUTE format(
-            'ALTER TABLE oconnor.%I ALTER COLUMN %I
+          EXECUTE format(
+              'ALTER TABLE oconnor.%I ALTER COLUMN %I
   TYPE entityid',
-            rec.table_name, rec.column_name
-        );
+              rec.table_name, rec.column_name
+          );
 END LOOP;
-
-    -- Recreate views from saved definitions
-FOR view_rec IN
-SELECT viewname, definition FROM _oconnor_view_defs
-    LOOP
-    EXECUTE format('CREATE OR REPLACE VIEW oconnor.%I AS %s', view_rec.viewname, view_rec.definition);
-END LOOP;
-
-DROP TABLE _oconnor_view_defs;
 END $$;
