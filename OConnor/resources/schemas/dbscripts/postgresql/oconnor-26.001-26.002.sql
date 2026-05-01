c22db367ccfbafd28d20dfbf059e5c7dba5c18f7
Adam Rauch <adam@labkey.com>
2026-05-01 09:07:10 -0700

    Comment

--- OConnor/resources/schemas/dbscripts/postgresql/oconnor-26.001-26.002.sql
+++ OConnor/resources/schemas/dbscripts/postgresql/oconnor-26.001-26.002.sql
@@ -3,5 +3,5 @@ DROP INDEX oconnor.key;
 -- Dropping experiment_db_experiment_number_key [experiment_number] because it overlaps with pk_experiment_db [experiment_number]
 DROP INDEX oconnor.experiment_db_experiment_number_key;
 -- Dropping unique index because pk_experiment_db [experiment_number] overlaps it with a smaller column set. Not bothering
--- to convert this unique index to a non-unique index because it has 11 columns, which is an absurd.
+-- to convert this unique index to a non-unique index because it has 11 columns, which is absurd.
 DROP INDEX oconnor.experiment_db_experiment_number_created_createdby_description_t;
