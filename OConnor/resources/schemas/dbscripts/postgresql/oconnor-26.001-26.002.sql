9d42268cc3d0aaf6882f585afb73c790264671a6
Adam Rauch <adam@labkey.com>
2026-05-01 08:28:45 -0700

    Don't recreate 11-column index

--- OConnor/resources/schemas/dbscripts/postgresql/oconnor-26.001-26.002.sql
+++ OConnor/resources/schemas/dbscripts/postgresql/oconnor-26.001-26.002.sql
@@ -2,6 +2,6 @@
 DROP INDEX oconnor.key;
 -- Dropping experiment_db_experiment_number_key [experiment_number] because it overlaps with pk_experiment_db [experiment_number]
 DROP INDEX oconnor.experiment_db_experiment_number_key;
--- Converting experiment_db_experiment_number_created_createdby_description_t [experiment_number, created, createdby, description, type, parents, workbook, container, modifiedby, modified, comments] from unique to non-unique index because pk_experiment_db [experiment_number] overlaps it with a smaller column set
+-- Dropping unique index because pk_experiment_db [experiment_number] overlaps it with a smaller column set. Not bothering
+-- to convert this unique index to a non-unique index because it has 11 columns, which is an absurd.
 DROP INDEX oconnor.experiment_db_experiment_number_created_createdby_description_t;
-CREATE INDEX experiment_db_experiment_number_created_createdby_description_t ON oconnor.experiment_db(experiment_number, created, createdby, description, type, parents, workbook, container, modifiedby, modified, comments);
