fd0a34ce88a96f7dc6b064810d8b2dd323b912f4
Adam Rauch <adam@labkey.com>
2026-05-01 12:51:20 -0700

    Drop redundant indices (#196)

---
+++ OConnor/resources/schemas/dbscripts/postgresql/oconnor-26.001-26.002.sql
@@ -0,0 +1,7 @@
+-- Dropping key [key] because it overlaps with inventorytemp_pkey [key]
+DROP INDEX oconnor.key;
+-- Dropping experiment_db_experiment_number_key [experiment_number] because it overlaps with pk_experiment_db [experiment_number]
+DROP INDEX oconnor.experiment_db_experiment_number_key;
+-- Dropping unique index because pk_experiment_db [experiment_number] overlaps it with a smaller column set. Not bothering
+-- to convert this unique index to a non-unique index because it has 11 columns, which is absurd.
+DROP INDEX oconnor.experiment_db_experiment_number_created_createdby_description_t;
